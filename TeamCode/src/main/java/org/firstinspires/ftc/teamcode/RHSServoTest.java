736e22fc56aea7f07d4bba64ce6951642f033d17
rhsftc <ronsearle@hotmail.com>
2022-09-30 13:16:00 -0500

    Servo changes to try things.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSServoTest.java
@@ -5,11 +5,13 @@ import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 @TeleOp(name = "FTCLib Servo Test", group = "Test")
 //@Disabled
 public class RHSServoTest extends LinearOpMode {
     private final double MAX_ANGLE = 180;
-    private final double MIN_ANGLE = 0;
+    private final double MIN_ANGLE = -180;
     private ServoEx servo;
 
     @Override
@@ -19,9 +21,11 @@ public class RHSServoTest extends LinearOpMode {
         waitForStart();
         while (opModeIsActive()) {
             if (gamepad1.dpad_left) {
-                servo.rotateBy(10);
+                servo.rotateByAngle(100, AngleUnit.DEGREES);
             } else if (gamepad1.dpad_right) {
-                servo.rotateBy(-10);
+                servo.rotateByAngle(-100, AngleUnit.DEGREES);
+            } else if (gamepad1.dpad_down) {
+                servo.setPosition(.5);
             }
         }
     }
