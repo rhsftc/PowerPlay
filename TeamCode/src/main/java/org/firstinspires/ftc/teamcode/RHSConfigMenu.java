031bf2839766a9f30459a99e6fb1d0ec45e71f3c
rhsftc <ronsearle@hotmail.com>
2022-10-06 13:33:28 -0500

    Added more speech to the config menu. The serve test has some more test options.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSConfigMenu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSConfigMenu.java
@@ -30,12 +30,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 /**
  * This file contains an example of an iterative (Non-Linear) "OpMode".
@@ -43,21 +39,20 @@ import com.qualcomm.robotcore.util.Range;
  * The names of OpModes appear on the menu of the FTC Driver Station.
  * When a selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
- *
+ * <p>
  * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
  * It includes all the skeletal structure that all iterative OpModes contain.
- *
+ * <p>
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Options Menu", group="Autonomous")
+@Autonomous(name = "Options Menu", group = "Autonomous")
 //@Disabled
-public class RHSConfigMenu extends OpMode
-{
+public class RHSConfigMenu extends OpMode {
+    AutonomousConfiguration autonomousConfiguration = new AutonomousConfiguration();
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    AutonomousConfiguration autonomousConfiguration = new AutonomousConfiguration();
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -81,7 +76,11 @@ public class RHSConfigMenu extends OpMode
     @Override
     public void start() {
         if (!autonomousConfiguration.getReadyToStart()) {
-            telemetry.addData("Alert","Not ready to start!");
+            telemetry.addData("Alert", "Not ready to start!");
+            telemetry.speak("Not ready to start!");
+            runtime.reset();
+            while (runtime.seconds() < 2) {
+            }
             requestOpModeStop();
         }
         runtime.reset();
