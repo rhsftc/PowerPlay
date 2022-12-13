0be95597d85e241a64473ae0a96811966b2b0b49
rhsftc <ronsearle@hotmail.com>
2022-12-13 10:36:54 -0600

    Removed some commented code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSAutoDriveByGyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSAutoDriveByGyro.java
@@ -444,13 +444,9 @@ public class RHSAutoDriveByGyro extends LinearOpMode {
         telemetry.addData("Wheel Velocities L:R", "%6.2f:%6.2f\n", leftVelocity, rightVelocity);
         // Retrieve Rotational Angles and Velocities
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-//        AngularVelocity angularVelocity = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
         telemetry.addData("Yaw (Z)", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
         telemetry.addData("Pitch (X)", "%.2f Deg.", orientation.getPitch(AngleUnit.DEGREES));
         telemetry.addData("Roll (Y)", "%.2f Deg.\n", orientation.getRoll(AngleUnit.DEGREES));
-//        telemetry.addData("Yaw (Z) velocity", "%.2f Deg/Sec", angularVelocity.zRotationRate);
-//        telemetry.addData("Pitch (X) velocity", "%.2f Deg/Sec", angularVelocity.xRotationRate);
-//        telemetry.addData("Roll (Y) velocity", "%.2f Deg/Sec", angularVelocity.yRotationRate);
         telemetry.update();
     }
 
