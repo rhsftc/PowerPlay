45e705f921a2b9486802dbb769cddc265fb20f64
rhsftc <ronsearle@hotmail.com>
2023-01-04 15:50:06 -0600

    Added Hardware class for use in all op modes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSRobotHardware.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class RHSRobotHardware {    /* Declare OpMode members. */
+    private LinearOpMode rhsOpMode = null;   // gain access to methods in the calling OpMode.
+
+    public RHSRobotHardware(LinearOpMode opMode) {
+        rhsOpMode = opMode;
+    }
+
+    public void init() {
+
+    }
+}
