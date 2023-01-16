072073974dc08b746ddc902a901ea2c2459f762f
rhsftc <ronsearle@hotmail.com>
2023-01-16 15:06:45 -0600

    Testing SimpleServo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSServoTest.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 @TeleOp(name = "FTCLib Servo", group = "test")
 //@Disabled
 public class RHSServoTest extends LinearOpMode {
-    private final double MAX_ANGLE = 360;
+    private final double MAX_ANGLE = 45;
     private final double MIN_ANGLE = 0;
     private SimpleServo servo;
 
@@ -17,19 +17,19 @@ public class RHSServoTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         servo = new SimpleServo(hardwareMap, "servo1", MIN_ANGLE, MAX_ANGLE);
         servo.setRange(MIN_ANGLE, MAX_ANGLE, AngleUnit.DEGREES);
-        servo.setInverted(true);
+        servo.setInverted(false);
         telemetry.addData("Servo", servo.getDeviceType());
         telemetry.update();
         waitForStart();
         while (opModeIsActive()) {
             if (gamepad1.dpad_left) {
-                servo.turnToAngle(270, AngleUnit.DEGREES);
+                servo.turnToAngle(40, AngleUnit.DEGREES);
             } else if (gamepad1.dpad_right) {
-                servo.turnToAngle(90, AngleUnit.DEGREES);
+                servo.turnToAngle(10, AngleUnit.DEGREES);
             } else if (gamepad1.dpad_down) {
-                servo.turnToAngle(180, AngleUnit.DEGREES);
+                servo.turnToAngle(20, AngleUnit.DEGREES);
             } else if (gamepad1.dpad_up) {
-                servo.turnToAngle(360, AngleUnit.DEGREES);
+                servo.turnToAngle(0, AngleUnit.DEGREES);
             } else if (gamepad1.x) {
                 servo.setPosition(0);
             } else if (gamepad1.y) {
