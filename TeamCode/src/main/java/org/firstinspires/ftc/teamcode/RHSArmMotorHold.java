2981235eb05a7aa2236d56c772e9cd1e9695f598
rhsftc <ronsearle@hotmail.com>
2023-01-23 14:20:02 -0600

    Arm code with FTCLib Pidf and SDK Pid code works.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSArmMotorHold.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RHSArmMotorHold.java
@@ -32,11 +32,14 @@ package org.firstinspires.ftc.teamcode;
 import com.arcrobotics.ftclib.controller.wpilibcontroller.ElevatorFeedforward;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 @TeleOp(name = "Arm Hold", group = "test")
 //@Disabled
@@ -49,48 +52,51 @@ public class RHSArmMotorHold extends LinearOpMode {
     static final int HOME_POSITION = 1;
     static final int CONE_HEIGHT = 5;
     static final int ADJUST_ARM_INCREMENT = 1;
-    static final double MAX_POWER = 0.4;
+    static final double MAX_POWER = 1;
     // Declare OpMode members.
+    private Datalog dataLog;
     private ElapsedTime runTime;
-    private MotorEx armMotor = null;
+    private DcMotorEx armMotor = null;
     private ElevatorFeedforward armFeedForward;
     private GamepadEx gamePadArm;
     private int armTarget = 0;
     private int armPosition = 0;
     private ArmPosition selectedPosition;
     private double armVelocity = 0;
-    private double armCorrectedVelocity = 0;
-    private double armAcceleration = 0;
-    private boolean isAtTarget = false;
+    private double armCurrent = 0;
+    private boolean isBusy = false;
     // These are set in init.
-    private double armCountsPerMotorRev = 0;
+    private double armCountsPerMotorRev = 2781;
     private double armCountsPerInch = 0;
 
     @Override
     public void runOpMode() {
         runTime = new ElapsedTime();
 
+        dataLog = new Datalog("armmotorhold");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        armMotor = new MotorEx(hardwareMap, "leftbackdrive", Motor.GoBILDA.RPM_435);
-        armFeedForward = new ElevatorFeedforward(10, 20, 0);
-        armCountsPerMotorRev = armMotor.ACHIEVABLE_MAX_TICKS_PER_SECOND;
+        armMotor = hardwareMap.get(DcMotorEx.class, "leftbackdrive");
+        armMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        armFeedForward = new ElevatorFeedforward(8, 20, 0);
         armCountsPerInch = ((armCountsPerMotorRev * ARM_DRIVE_REDUCTION) / (ARM_WHEEL_DIAMETER_INCHES * 3.145));
-        armMotor.setInverted(false);
-        armMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+
         gamePadArm = new GamepadEx(gamepad2);
 
         // Wait for start
         waitForStart();
+
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         runTime.reset();
-        armMotor.resetEncoder();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive() && !isStopRequested()) {
             gamePadArm.readButtons();
             ProcessArm();
-//            sendTelemetry();
+            sendTelemetry();
         }
     }
 
@@ -100,9 +106,8 @@ public class RHSArmMotorHold extends LinearOpMode {
         telemetry.addData("Target Position", "%d", armTarget);
         telemetry.addData("Current Position", armPosition);
         telemetry.addData("Velocity", armVelocity);
-        telemetry.addData("Corrected Velocity", armCorrectedVelocity);
-        telemetry.addData("Acceleration", armAcceleration);
-        telemetry.addData("At target", isAtTarget);
+        telemetry.addData("Current", armCurrent);
+        telemetry.addData("Busy", isBusy);
         telemetry.update();
     }
 
@@ -172,23 +177,28 @@ public class RHSArmMotorHold extends LinearOpMode {
         // Prevent arm moving below HOME_POSITION
         armTarget = Math.max(armTarget, HOME_POSITION * (int) armCountsPerInch);
         armMotor.setTargetPosition(armTarget);
-        armMotor.setPositionCoefficient(.003);
-        armMotor.setPositionTolerance(25);
-        armMotor.setTargetPosition(armTarget);
-        armMotor.setRunMode(Motor.RunMode.PositionControl);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setVelocityPIDFCoefficients(1.26, 0.126, 0, 12.6);
+        armMotor.setPositionPIDFCoefficients(8);
 
-        while (!armMotor.atTargetPosition() && !isStopRequested()) {
-            armMotor.set(MAX_POWER);
+        while (armMotor.isBusy() && !isStopRequested()) {
+            armMotor.setVelocity(2800);
+            armMotor.setPower(armFeedForward.calculate(2800));
             armPosition = armMotor.getCurrentPosition();
             armVelocity = armMotor.getVelocity();
-            armCorrectedVelocity = armMotor.getCorrectedVelocity();
-            armAcceleration = armMotor.getAcceleration();
-            isAtTarget = armMotor.atTargetPosition();
+            armCurrent = armMotor.getCurrent(CurrentUnit.AMPS);
+            isBusy = armMotor.isBusy();
+            logData();
             sendTelemetry();
         }
+    }
 
-//        armMotor.setVelocity(0);
-//        armMotor.stopMotor();
+    public void logData() {
+        dataLog.target.set(armTarget);
+        dataLog.position.set(armPosition);
+        dataLog.velocity.set(armVelocity);
+        dataLog.current.set(armCurrent);
+        dataLog.writeLine();
     }
 
     public enum ArmPosition {
@@ -198,5 +208,48 @@ public class RHSArmMotorHold extends LinearOpMode {
         HIGH,
         ADJUST_UP,
         ADJUST_DOWN
+    }
+
+    /*
+     * This class encapsulates all the fields that will go into the datalog.
+     */
+    public static class Datalog {
+        // The underlying datalogger object - it cares only about an array of loggable fields
+        private final Datalogger datalogger;
+
+        // These are all of the fields that we want in the datalog.
+        // Note that order here is NOT important. The order is important in the setFields() call below
+        public Datalogger.GenericField velocity = new Datalogger.GenericField("Velocity");
+        public Datalogger.GenericField target = new Datalogger.GenericField("Target");
+        public Datalogger.GenericField position = new Datalogger.GenericField("Position");
+        public Datalogger.GenericField current = new Datalogger.GenericField("Current");
+
+        public Datalog(String name) {
+            // Build the underlying datalog object
+            datalogger = new Datalogger.Builder()
+
+                    // Pass through the filename
+                    .setFilename(name)
+
+                    // Request an automatic timestamp field
+                    .setAutoTimestamp(Datalogger.AutoTimestamp.DECIMAL_SECONDS)
+
+                    // Tell it about the fields we care to log.
+                    // Note that order *IS* important here! The order in which we list
+                    // the fields is the order in which they will appear in the log.
+                    .setFields(
+                            target,
+                            position,
+                            velocity,
+                            current
+                    )
+                    .build();
+        }
+
+        // Tell the datalogger to gather the values of the fields
+        // and write a new line in the log.
+        public void writeLine() {
+            datalogger.writeLine();
+        }
     }
 }
